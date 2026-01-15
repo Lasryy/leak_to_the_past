a63d023371c887b839f4d9991b1ad9c7958c67a4
Lasry BESKIWIN <lasrybeskiwin@gmail.com>
2026-01-15 15:12:13 +0100

    ajout de constantes dans settings

--- settings.py
+++ settings.py
@@ -8,3 +8,17 @@ FPS = 60
 
 # Titre du jeu
 TITLE = "Leak To The Past"
+
+# Taille des tuiles
+TILESIZE = 64
+
+# Couleurs de base (RGB)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+
+# Couleurs néon
+CYAN = (0, 255, 255)
+MAGENTA = (255, 0, 255)
